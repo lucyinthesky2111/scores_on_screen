994f15f367b09757c005be7319794c84dc6234c5
lucyinthesky2111 <lucy.kidner@outlook.com>
2026-05-10 20:39:32 +0100

    Intall Whitenoise version 6.5.0

--- scores_on_screen/settings.py
+++ scores_on_screen/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$!^1pz1j-ktm@ve@t=xylzf&%pwy0b0gj^sci)8ja2pv!xo*$d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com',]
 
