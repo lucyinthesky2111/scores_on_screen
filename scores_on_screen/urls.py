9d4e0929cafeb50c6d91489edad23eb9012fbb9a
lucyinthesky2111 <lucy.kidner@outlook.com>
2026-05-10 19:41:06 +0100

    Create scores app and add relevant view and url code

--- scores_on_screen/urls.py
+++ scores_on_screen/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from scores import views as score_views
 
 urlpatterns = [
+    path('', score_views.scores, name='scores'),
     path('admin/', admin.site.urls),
+   
 ]
