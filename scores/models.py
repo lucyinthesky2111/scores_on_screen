2a4cd88e668a5a96d63a75a75b00e276a74ef432
lucyinthesky2111 <lucy.kidner@outlook.com>
2026-05-12 22:06:02 +0100

    Create Score and Composer models

--- scores/models.py
+++ scores/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Score(models.Model):
+    score_name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True)
+    composer = models.ForeignKey('Composer', on_delete=models.PROTECT)
+    youtube_url = models.URLField()
+    score_description = models.TextField()
+    score_runtime = models.DurationField()
+    release_date = models.DateField()
+    score_created_at = models.DateTimeField(auto_now_add=True)
+    score_updated_at = models.DateTimeField(auto_now=True)
+    score_deleted = models.BooleanField("deleted", default=False)
+    status = models.IntegerField(choices=STATUS, default=0)
+    
+    
+class Composer(models.Model):
+    composer_name = models.CharField(max_length=255)
+    composer_birth_date = models.DateField()
+    composer_death_date = models.DateField(null=True, blank=False)
+    
+
+    def __str__(self):
+        return self.composer_name
