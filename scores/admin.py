082f6387c8f53d43990bd957f71381e5385d9bb7
lucyinthesky2111 <lucy.kidner@outlook.com>
2026-05-17 18:32:00 +0100

    Add code to admin.py to import and register Track model

--- scores/admin.py
+++ scores/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Score, Composer
+from .models import Score, Composer, Track
 
 # Register your models here.
 admin.site.register(Score)
 admin.site.register(Composer)
+admin.site.register(Track)
+
